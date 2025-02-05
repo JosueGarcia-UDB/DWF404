f91d69d9c5897129a1ef9e6e023aa38b37073e4b
JosueGarcia-UDB <ge240098@alumno.udb.edu.sv>
2025-02-05 08:14:48 -0600

    Add Guia#3 - Inyección de dependencias

---
+++ G2-spring-di/src/main/java/sv/edu/udb/G2_spring_di/G2SpringDiApplication.java
@@ -0,0 +1,13 @@
+package sv.edu.udb.G2_spring_di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G2SpringDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G2SpringDiApplication.class, args);
+	}
+
+}
